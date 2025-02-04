a4c2a5583e2f5bed3cd3f8ce02b16a5019af8014
valentinfrlch <valentin.froehlich@gmail.com>
2025-02-04 21:10:05 +0100

    Add Popup

--- llmvision-card.js
+++ llmvision-card.js
@@ -1,6 +1,5 @@
 import { getIcon } from './helpers.js';
 
-
 class LLMVisionCard extends HTMLElement {
 
     config;
@@ -36,6 +35,7 @@ class LLMVisionCard extends HTMLElement {
                         height: auto;
                         z-index: 2;
                         margin-bottom: 10px;
+                        cursor: pointer;
                     }
 
                     .event-container:last-child {
@@ -91,6 +91,7 @@ class LLMVisionCard extends HTMLElement {
         }
 
         const events = (calendarEntity.attributes.events || []).slice().reverse();
+        const summaries = (calendarEntity.attributes.summaries || []).slice().reverse();
         const keyFrames = (calendarEntity.attributes.key_frames || []).slice().reverse();
         const cameraNames = (calendarEntity.attributes.camera_names || []).slice().reverse();
         const startTimes = (calendarEntity.attributes.starts || []).slice().reverse();
@@ -99,36 +100,136 @@ class LLMVisionCard extends HTMLElement {
         this.content.innerHTML = '';
 
         // Add events and key frames for the specified number of events
+        let lastDate = '';
+
         for (let i = 0; i < Math.min(numberOfEvents, events.length); i++) {
             const event = events[i];
+            const summary = summaries[i];
             const startTime = startTimes[i];
             const date = new Date(startTime);
+            const options = { month: 'short', day: 'numeric' };
+            const formattedDate = date.toLocaleDateString('en-US', options);
             const hours = date.getHours().toString().padStart(2, '0');
             const minutes = date.getMinutes().toString().padStart(2, '0');
             const formattedTime = `${hours}:${minutes}`;
             const icon = getIcon(event);
             let keyFrame = keyFrames[i] || '';
             const cameraName = cameraNames[i] || '';
-            // formatted time • camera name if camera not "" else just formatted time
-            console.log(cameraName);
             const secondaryText = cameraName ? `${formattedTime} • ${cameraName}` : formattedTime;
-            console.log(secondaryText);
 
             keyFrame = keyFrame.replace('/config/www/', '/local/') + ".jpg";
 
-            this.content.innerHTML += `
-                <div class="event-container">
-                    <div class="icon-container">
-                        <ha-icon icon="${icon}"></ha-icon>
-                    </div>
-                    <div class="event-details">
-                        <h3>${event}</h3>
-                        <p>${secondaryText}</p>
-                    </div>
-                    <img src="${keyFrame}" alt="Key frame ${i + 1}" onerror="this.style.display='none'">
+            // Determine the date label
+            const today = new Date();
+            const yesterday = new Date(today);
+            yesterday.setDate(today.getDate() - 1);
+
+            let dateLabel;
+            if (date.toDateString() === today.toDateString()) {
+                dateLabel = 'Today';
+            } else if (date.toDateString() === yesterday.toDateString()) {
+                dateLabel = 'Yesterday';
+            } else {
+                dateLabel = formattedDate;
+            }
+
+            // Add the date if it's different from the last date
+            if (dateLabel !== lastDate) {
+                const dateHeader = document.createElement('div');
+                dateHeader.classList.add('date-header');
+                dateHeader.innerHTML = `<h2>${dateLabel}</h2>`;
+                this.content.appendChild(dateHeader);
+                lastDate = dateLabel;
+            }
+
+            const eventContainer = document.createElement('div');
+            eventContainer.classList.add('event-container');
+            eventContainer.innerHTML = `
+                <div class="icon-container">
+                    <ha-icon icon="${icon}"></ha-icon>
                 </div>
+                <div class="event-details">
+                    <h3>${event}</h3>
+                    <p>${secondaryText}</p>
+                </div>
+                <img src="${keyFrame}" alt="Key frame ${i + 1}" onerror="this.style.display='none'">
             `;
+
+            console.log(`Attaching click event for event: ${event}, dateLabel: ${dateLabel}`);
+
+
+            eventContainer.addEventListener('click', () => {
+                console.log(`Event clicked: ${event}, dateLabel: ${dateLabel}`);
+                this.showPopup(event, summary, startTime, keyFrame, cameraName);
+            });
+
+            this.content.appendChild(eventContainer);
         }
+    }
+
+    showPopup(event, summary, startTime, keyFrame, cameraName) {
+        const date = new Date(startTime);
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const formattedTime = `${hours}:${minutes}`;
+        const secondaryText = cameraName ? `${formattedTime} • ${cameraName}` : formattedTime;
+        const eventDetails = `
+            <div>
+                <h2 style="margin-bottom:0">${event}</h2>
+                <p><span style="font-weight: bold; color: var(--secondary-text-color);">${secondaryText}</span></p>
+                <p>${summary}</p>
+                <img src="${keyFrame}" alt="Key frame" style="width: 100%; height: auto; border-radius: 12px;">
+            </div>
+        `;
+
+        const popup = document.createElement('div');
+        popup.innerHTML = `
+            <div class="popup-overlay">
+                <div class="popup-content">
+                    <button class="close-popup">&times;</button>
+                    ${eventDetails}
+                </div>
+            </div>
+            <style>
+                .popup-overlay {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    background: rgba(0, 0, 0, 0.5);
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    z-index: 1000;
+                }
+                .popup-content {
+                    position: relative;
+                    background: var(--ha-card-background, var(--card-background-color, black));
+                    color: var(--primary-text-color);
+                    padding: 20px;
+                    border-radius: var(--border-radius, 15px);
+                    max-width: 500px;
+                    width: 100%;
+                }
+                .close-popup {
+                    position: absolute;
+                    top: 10px;
+                    right: 10px;
+                    background: none;
+                    border: none;
+                    font-size: 30px;
+                    cursor: pointer;
+                    color: var(--primary-text-color);
+                }
+            </style>
+        `;
+
+        document.body.appendChild(popup);
+
+        popup.querySelector('.close-popup').addEventListener('click', () => {
+            document.body.removeChild(popup);
+        });
     }
 
     static getStubConfig() {
