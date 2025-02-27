1416ca84cade32cf893badca458b4b6bdc4eb400
valentinfrlch <valentin.froehlich@gmail.com>
2025-02-27 17:52:52 +0100

    Update colors

--- dist/colors.js
+++ dist/colors.js
@@ -1,7 +1,7 @@
 export const colors = {
     "categories": {
         "people": {
-            "colors": { "background": "#FFAD42", "icon": "#FFEFDA" } // Orange, lightorange
+            "colors": { "background": "#FFAD42", "icon": "#FFFBF5" } // Orange, lightorange
         },
         "vehicles": {
             "colors": { "background": "#494949", "icon": "#E4E4E4" } // Gray, white
@@ -13,7 +13,7 @@ export const colors = {
             "colors": { "background": "#D6946D", "icon": "#EBDBD7" } // Orange, yellow
         },
         "entities": {
-            "colors": { "background": "#1c6795", "icon": "#7fcfff" } // dark blue, blue
+            "colors": { "background": "#1c6795", "icon": "#D1EFFF" } // dark blue, blue
         },
         "nature": {
             "colors": { "background": "#00952F", "icon": "#42FF75" } // Green, lightgreen
