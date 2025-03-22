73e2739b421f6122c85868d3cbc5446b43ef707d
catpersec <45229683+catpersec@users.noreply.github.com>
2025-03-22 07:42:41 +0100

    cleanup

--- README.md
+++ README.md
@@ -29,39 +29,34 @@
 <img src="https://github.com/user-attachments/assets/97f6e608-bdf3-44d1-89f1-fd89cda7b764" data-canonical-src="https://gyazo.com/eb5c5741b6a9a16c692170a41a49c858.png" width="50%" height="auto" />
 
 ## Prerequisites
-
 1. [LLM Vision](https://github.com/valentinfrlch/ha-llmvision) set up in Home Assistant
 2. Timeline provider set up in LLM Vision
 3. Blueprint or Automation to add events to the timeline
 
 ## Documentation
-
 <a href="https://llm-vision.gitbook.io/getting-started/setup/timeline-card-beta"><img src="https://img.shields.io/badge/Documentation-blue?style=for-the-badge&logo=gitbook&logoColor=white&color=18bcf2"/> </a>
 
 ## Installation
-
 Add the repository to HACS and install the LLM Vision card using this link:
 [![Open your Home Assistant instance and open a repository inside the Home Assistant Community Store.](https://my.home-assistant.io/badges/hacs_repository.svg)](https://my.home-assistant.io/redirect/hacs_repository/?owner=valentinfrlch&repository=llmvision-card&category=plugin)
 
 Alternatively you can add the url of this repository to the custom respositories list in HACS.
 
 ## Setup
-
 1. Install the card through HACS
 2. Reload
 3. Add the card to your dashboard
 
 ## Configuration
 
-| Parameter        | Description                                                                                  | Default                      |
-| ---------------- | -------------------------------------------------------------------------------------------- | ---------------------------- |
-| number_of_events | How many events to show                                                                      | 5                            |
-| calendar_entity  | LLM Vision Timeline Entity (needs to be set up in LLM Vision Settings first)                 | calendar.llm_vision_timeline |
-| refresh_interval | Refresh Interval (in seconds)                                                                | 10                           |
-| language         | Language used for UI and generate icons (supports: `en`, `de`, `nl`, `fr`, `es`, `pt`, `pl`) | `en`                         |
+| Parameter         | Description                                                                              | Default                      |
+|-------------------|------------------------------------------------------------------------------------------|------------------------------|
+| number_of_events  | How many events to show                                                                  | 5                            |
+| calendar_entity   | LLM Vision Timeline Entity (needs to be set up in LLM Vision Settings first)             | calendar.llm_vision_timeline |
+| refresh_interval  | Refresh Interval (in seconds)                                                            | 10                           |
+| language          | Language used for UI and generate icons (supports: `en`, `de`, `nl`, `fr`, `es`, `pt`, `pl`)         | `en`                         |
 
 ## Support
-
 You can support this project by starring this GitHub repository. If you want, you can also buy me a coffee here:  
 <br>
 <a href="https://www.buymeacoffee.com/valentinfrlch"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=valentinfrlch&button_colour=FFDD00&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=ffffff" /></a>
--- dist/helpers.js
+++ dist/helpers.js
@@ -1,68 +1,62 @@
-import { colors } from "./colors.js";
-import { fr } from "./fr.js";
-import { de } from "./de.js";
-import { nl } from "./nl.js";
-import { en } from "./en.js";
-import { es } from "./es.js";
-import { pt } from "./pt.js";
-import { pl } from "./pl.js";
+import { colors } from './colors.js';
+import { fr } from './fr.js';
+import { de } from './de.js';
+import { nl } from './nl.js';
+import { en } from './en.js';
+import { es } from './es.js';
+import { pt } from './pt.js';
+import { pl } from './pl.js';
 
-export function getIcon(title, lang = "en") {
-  let categories;
+export function getIcon(title, lang = 'en') {
+    let categories;
 
-  try {
-    if (lang === "de") {
-      categories = de.categories;
-    } else if (lang === "nl") {
-      categories = nl.categories;
-    } else if (lang === "en") {
-      categories = en.categories;
-    } else if (lang === "fr") {
-      categories = fr.categories;
-    } else if (lang === "es") {
-      categories = es.categories;
-    } else if (lang === "pt") {
-      categories = pt.categories;
-    } else if (lang === "pl") {
-      categories = pl.categories;
-    } else {
-      throw new Error(`Unsupported language: ${lang}`);
-    }
-
-    for (const categoryName of Object.keys(categories)) {
-      const category = categories[categoryName];
-      const categoryColors = colors.categories[categoryName].colors;
-      const { objects } = category;
-      for (const [key, icon] of Object.entries(objects)) {
-        const regex = new RegExp(`\\b${key}s?\\b`, "i");
-        if (regex.test(title)) {
-          return {
-            icon,
-            backgroundColor: categoryColors.background,
-            iconColor: categoryColors.icon,
-          };
+    try {
+        if (lang === 'de') {
+            categories = de.categories;
+        } else if (lang === 'nl') {
+            categories = nl.categories;
+        } else if (lang === 'en') {
+            categories = en.categories;
+        } else if (lang === 'fr') {
+            categories = fr.categories;
+        } else if (lang === 'es') {
+            categories = es.categories;
+        } else if (lang === 'pt') {
+            categories = pt.categories;
+        } else if (lang === 'pl') {
+            categories = pl.categories;
+        } else {
+            throw new Error(`Unsupported language: ${lang}`);
         }
-      }
-    }
-  } catch (error) {
-    console.error("Error getting icon:", error);
-  }
 
-  return { icon: "mdi:cube-scan", backgroundColor: "gray", iconColor: "white" }; // Default icon and colors if no keyword is found
+        for (const categoryName of Object.keys(categories)) {
+            const category = categories[categoryName];
+            const categoryColors = colors.categories[categoryName].colors;
+            const { objects } = category;
+            for (const [key, icon] of Object.entries(objects)) {
+                const regex = new RegExp(`\\b${key}s?\\b`, 'i');
+                if (regex.test(title)) {
+                    return { icon, backgroundColor: categoryColors.background, iconColor: categoryColors.icon };
+                }
+            }
+        }
+    } catch (error) {
+        console.error('Error getting icon:', error);
+    }
+
+    return { icon: 'mdi:cube-scan', backgroundColor: 'gray', iconColor: 'white' }; // Default icon and colors if no keyword is found
 }
 
 const translations = {
-  en: en.text,
-  de: de.text,
-  nl: nl.text,
-  fr: fr.text,
-  es: es.text,
-  pt: pt.text,
-  pl: pl.text,
+    en: en.text,
+    de: de.text,
+    nl: nl.text,
+    fr: fr.text,
+    es: es.text,
+    pt: pt.text,
+    pl: pl.text,
 };
 
 export function translate(key, language) {
-  return translations[language] && translations[language][key]
-    ? translations[language][key]
-    : translations["en"][key];
+    return translations[language] && translations[language][key] ? translations[language][key] : translations['en'][key];
 }
