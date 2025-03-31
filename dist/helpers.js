d6c0d85f630cdf1fe9a0a0c9f5aa5ecf8a1216d0
adnansarajlic <adnansarajlic@gmail.com>
2025-03-31 08:41:15 +0200

    Added Swedish translation

--- dist/helpers.js
+++ dist/helpers.js
@@ -5,6 +5,7 @@ import { nl } from './nl.js';
 import { en } from './en.js';
 import { es } from './es.js';
 import { pt } from './pt.js';
+import { sv } from './sv.js';
 
 export function getIcon(title, lang = 'en') {
     let categories;
@@ -22,6 +23,8 @@ export function getIcon(title, lang = 'en') {
             categories = es.categories;
         } else if (lang === 'pt') {
             categories = pt.categories;
+        } else if (lang === 'sv') {
+            categories = sv.categories;
         } else {
             throw new Error(`Unsupported language: ${lang}`);
         }
@@ -51,6 +54,7 @@ const translations = {
     fr: fr.text,
     es: es.text,
     pt: pt.text,
+    pt: sv.text,
 };
 
 export function translate(key, language) {
