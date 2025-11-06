e83c0a2dd97db16cf98301f276c5217cc9f12139
Lestat680 <78495815+Lestat680@users.noreply.github.com>
2025-11-06 09:57:05 +0100

    Create da.js

---
+++ dist/da.js
@@ -0,0 +1,68 @@
+export const da = {
+    "categories": {
+        "people": {
+            "objects": {
+                "person": "mdi:walk",
+                "personer": "mdi:walk",
+                "barn": "mdi:walk",
+                "kvinde": "mdi:walk",
+                "mand": "mdi:walk",
+                "menneske": "mdi:walk"
+            }
+        },
+        "vehicles": {
+            "objects": {
+                "kurer": "mdi:truck-delivery",
+                "postbud": "mdi:truck-delivery",
+                "cykel": "mdi:bike",
+                "motorcykel": "mdi:motorbike",
+                "bus": "mdi:bus",
+                "bil": "mdi:car",
+                "varevogn": "mdi:car",
+                "suv": "mdi:car",
+                "kÃ¸retÃ¸j": "mdi:car",
+                "lastbil": "mdi:truck"
+            }
+        },
+        "animals": {
+            "objects": {
+                "hund": "mdi:dog",
+                "kat": "mdi:cat",
+                "fugl": "mdi:duck",
+                "kÃ¦Ã¦ledyr": "mdi:dog",
+                "dyr": "mdi:dog"
+            }
+        },
+        "packages": {
+            "objects": {
+                "kasse": "mdi:package-variant-closed",
+                "forsendelse": "mdi:package-variant-closed",
+                "pakke": "mdi:package-variant-closed",
+                "brev": "mdi:email"
+            }
+        },
+        "entities": {
+            "objects": {
+                "kamera": "mdi:cctv",
+                "sensor": "mdi:access-point",
+                "lys": "mdi:lightbulb",
+                "nÃ¸gle": "mdi:key",
+                "lÃ¥s": "mdi:lock",
+                "alarm": "mdi:alarm-light",
+                "hus": "mdi:home",
+                "indgang": "mdi:door-closed",
+                "dÃ¸r": "mdi:door-closed",
+                "vindue": "mdi:window-closed-variant"
+            }
+        },
+        "nature": {
+            "objects": {
+                "have": "mdi:flower",
+                "plante": "mdi:flower",
+                "blomst": "mdi:flower",
+                "trÃ¦": "mdi:tree"
+            }
+        }
+    },
+    "regex": "`\\b${key}(da)?\\b`, 'i'"
+};
